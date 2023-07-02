34ffadd75f55e928acd5e37d2781680279dd4181
Abdallah Hossam-Eldin <eng.abdallahhossam@outlook.com>
2023-07-02 08:21:53 -0700

    Add notes for design techniques

---
+++ Multithreaded Programming with C++/Design Techniques.cpp
@@ -0,0 +1,21 @@
+// * Design Techniques
+
+// ? What is the difference between Design technique and Design pattern?
+// * Design technique is a strategy particular to C++ language aims to overcome n annoying feature or language deficiency and and frequently used 
+// *    solve a common C++ problem in the code 
+
+// * Design pattern is more general strategy for OOP design and applicable with any OOP language.
+// TODO: (complete the defintion of design pattern) 
+
+
+// C language 
+// keywords are few
+// language feature are minimal 
+// Behaviors are well-defined 
+
+// * So it is easy to memorize all language features
+// ! this is not the case with C++, even C++ experts need to look thing up sometimes ^^
+
+
+
+
